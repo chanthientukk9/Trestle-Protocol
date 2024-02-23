c911dab94feaf53c15ace75297830e99dd4012c3
Tuan Chu <chanthientukk9@gmail.com>
2024-02-23 15:19:22 +0700

    feat: update coming soon ui

--- src/pages/ComingSoonPage/ComingSoonPage.tsx
+++ src/pages/ComingSoonPage/ComingSoonPage.tsx
@@ -4,7 +4,7 @@ import PageWrapper from "../../components/PageWrapper";
 function ComingSoonPage() {
   return (
     <PageWrapper>
-      <section className="relative px-3 md:px-6 xl:px-0 md:top-0 md:pt-[160px] pt-[120px] w-full max-w-[1200px]">
+      <section className="relative px-3 md:px-6 xl:px-0 md:top-0 md:pt-[160px] pt-[120px] w-full max-w-[1200px] m-auto">
         <BasicCard className="text-center">
             <strong className="font-quicksand font-bold text-lg">Comming soon</strong>
         </BasicCard>
